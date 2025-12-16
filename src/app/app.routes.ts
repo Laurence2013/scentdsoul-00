9b202a921fd0543c75d2c5a657185779379dc4b4
Laurence2013 <lozza244@protonmail.com>
2025-12-16 20:06:42 +0000

    04-scentdsoul

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,24 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+	{
+		path: 'tabs',
+		loadComponent: () => import('./features/tabs/tabs.page').then(m => m.TabsPage),
+		children: [
+			{
+				path: 'home',
+				loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+			},
+			{
+				path: '',
+				redirectTo: 'home',
+				pathMatch: 'full',
+			}
+		]
+	},
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tabs/home',
     pathMatch: 'full',
   },
 ];
