c92cd4fb15f634c30ab86d36ccbab727f0c7cb93
Laurence2013 <lozza244@protonmail.com>
2025-12-25 13:49:22 +0000

    25-commit

--- projects/admin-panel/src/environments/environment.ts
+++ projects/admin-panel/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: process.env['API_KEY'],
-		authDomain: process.env['AUTH_DOMAIN'],
-		projectId: process.env['PROJECT_ID'],
-		storageBucket: process.env['STORAGE_BUCKET'],
-		messagingSenderId: process.env['MESSAGING_SENDER_ID'],
-		appId: process.env['APP_ID']
+		apiKey: process.env['NG_APP_API_KEY'],
+		authDomain: process.env['NG_APP_AUTH_DOMAIN'],
+		projectId: process.env['NG_APP_PROJECT_ID'],
+		storageBucket: process.env['NG_APP_STORAGE_BUCKET'],
+		messagingSenderId: process.env['NG_APP_MESSAGING_SENDER_ID'],
+		appId: process.env['NG_APP_APP_ID']
 	},
 	useEmulators: true
 }
