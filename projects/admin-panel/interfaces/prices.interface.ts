27ccbc9abe6ec8c82dff7c000ca286869615f949
Laurence2013 <lozza244@protonmail.com>
2025-12-31 16:49:04 +0000

    55a-commit

--- projects/admin-panel/interfaces/prices.interface.ts
+++ projects/admin-panel/interfaces/prices.interface.ts
@@ -1,7 +1,7 @@
 import { Timestamp } from '@angular/fire/firestore';
 import { SeasonalOffers } from './offers.interface';
 
-export type PlatformType = 'Web_Store' | 'Instagram' | 'TikTok' | 'eBay'
+export type PlatformType = 'Web_Store' | 'Instagram' | 'TikTok_Shop' | 'eBay'
 export type CurrencyCode = 'USD' | 'GBP' | 'EUR';
 
 export interface StorePrice {
