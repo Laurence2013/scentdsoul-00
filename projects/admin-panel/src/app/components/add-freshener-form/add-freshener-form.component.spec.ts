997ffc8604454ad839501c852b0c88fe864a18d2
Laurence2013 <lozza244@protonmail.com>
2026-01-05 20:01:24 +0000

    59d-commit

--- projects/admin-panel/src/app/components/add-freshener-form/add-freshener-form.component.spec.ts
+++ projects/admin-panel/src/app/components/add-freshener-form/add-freshener-form.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+
 import { AddFreshenerFormComponent } from './add-freshener-form.component';
 
 describe('AddFreshenerFormComponent', () => {
   let component: AddFreshenerFormComponent;
   let fixture: ComponentFixture<AddFreshenerFormComponent>;
 
+	const mockDialogRef = { close: jasmine.createSpy('close') };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [AddFreshenerFormComponent],
+			providers: [{
+				provide: DynamicDialogRef, useValue: mockDialogRef
+			}]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddFreshenerFormComponent);
@@ -16,7 +23,7 @@ describe('AddFreshenerFormComponent', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create AddFreshenerFormComponent', () => {
     expect(component).toBeTruthy();
   });
 });
--- projects/admin-panel/src/app/components/dashboard/dashboard.component.spec.ts
+++ projects/admin-panel/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { of } from 'rxjs';
 
 import { DashboardComponent } from './dashboard.component';
+
+import { DataService } from '../../services/firestore/data-service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+	const mockDataService = {
+		getCarAirFresheners: jasmine.createSpy('getCarAirFresheners').and.returnValue(of([])),
+		addNewCarAirFreshener00: jasmine.createSpy('addNewCarAirFreshener00').and.returnValue(of(null))
+	};
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [ DashboardComponent ],
+			providers: [
+				{provide: DataService, useValue: mockDataService},
+				provideAnimations()
+			]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
@@ -16,7 +29,7 @@ describe('DashboardComponent', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create DashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 });
